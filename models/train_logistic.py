5e1ed372096abc36c54461e61537f408f73c2388
Praneeth Sangnal <praneethsangnal8305@gmail.com>
2026-02-24 19:37:29 +0530

    done with the proj

---
+++ models/train_logistic.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from preprocessing.clean_titanic import clean_data
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+
+def train_model():
+
+    # ----------------------------
+    # Load Data
+    # ----------------------------
+    df = pd.read_csv("datasets/titanic.csv")
+
+    # ----------------------------
+    # Clean Data
+    # ----------------------------
+    df = clean_data(df)
+
+    # ----------------------------
+    # Separate Features & Target
+    # ----------------------------
+    X = df.drop("Survived", axis=1)
+    y = df["Survived"]
+
+    # ----------------------------
+    # Train-Test Split
+    # ----------------------------
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    # ----------------------------
+    # Train Logistic Regression
+    # ----------------------------
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
+
+    # ----------------------------
+    # Predictions
+    # ----------------------------
+    y_pred = model.predict(X_test)
+
+    # ----------------------------
+    # Evaluation
+    # ----------------------------
+    print("\nAccuracy:", accuracy_score(y_test, y_pred))
+
+    print("\nConfusion Matrix:")
+    print(confusion_matrix(y_test, y_pred))
+
+    print("\nClassification Report:")
+    print(classification_report(y_test, y_pred))
+
+
+if __name__ == "__main__":
+    train_model()
--- pandas/titanic_exploration.py
+++ pandas/titanic_exploration.py
@@ -5,7 +5,7 @@ df = pd.read_csv("../datasets/titanic.csv")
 
 # Basic inspection
 print("First 5 rows:\n")
-print(df.head())
+print(df.head(10))
 
 print("\nShape:", df.shape)
 
@@ -14,3 +14,4 @@ print("\nColumns:\n", df.columns)
 print("\nMissing values:\n", df.isnull().sum())
 
 print("\nData types:\n", df.dtypes)
+print("row*cols",df.size)
